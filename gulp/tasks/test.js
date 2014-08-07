9c8be6bce25e590ef3419698933b07f0a94d5972
sebastien <lorber.sebastien@gmail.com>
2014-08-07 18:30:30 +0200

    Add first jasmine tests

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -3,6 +3,6 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
 gulp.task('test', function () {
-    return gulp.src('test/atomTests.js')
+    return gulp.src('test/atomUtilsTests.js')
         .pipe(jasmine());
 });
