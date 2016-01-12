2b5f7ab385d321b3e845ac3b7fd27e02a6964dcd
sebastien <lorber.sebastien@gmail.com>
2016-01-12 17:40:43 +0100

    Conform event attributes to flux-standard-action

--- src/atomReactEvent.js
+++ src/atomReactEvent.js
@@ -4,9 +4,11 @@
 var Preconditions = require("./utils/preconditions");
 
 var AtomReactEvent = function(eventName,eventData) {
-    Preconditions.checkHasValue(eventName,"Event name is mandatory");
+    Preconditions.checkHasValue(eventName,"Event name/type is mandatory");
     this.name = eventName;
+    this.type = eventName;
     this.data = eventData;
+    this.payload = eventData;
     this.timestamp = new Date().getTime();
 };
 module.exports = AtomReactEvent;
